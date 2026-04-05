8a6dbd555e0ccb62d7632fdac4cd61e9801aa7d3
Youssef Mouad <imouad.you@gmail.com>
2026-04-05 17:44:40 +0100

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+
+    combinationSum(nums, target) {
+        const result = [];
+
+        function backtrack(start, combo, remaining) {
+            console.log(combo, "\n")
+            if (remaining === 0) {
+                result.push([...combo]);
+                return;
+            }
+
+            for (let i = start; i < nums.length; i++) {
+                if (nums[i] > remaining) continue;
+                combo.push(nums[i]);
+                backtrack(i, combo, remaining - nums[i]);
+                combo.pop();
+            }
+        }
+
+        backtrack(0, [], target);
+        return result;
+    }
+}
