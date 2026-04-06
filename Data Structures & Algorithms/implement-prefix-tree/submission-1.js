1069fad2a42bca075e6a510ae331838af4093827
Youssef Mouad <imouad.you@gmail.com>
2026-04-06 15:35:09 +0100

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.js
@@ -0,0 +1,62 @@
+class PrefixTree {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let index = word.charCodeAt(i) - 97;
+            if (!node.children[index]) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index]
+        }
+        node.endOfWord = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let index = word.charCodeAt(i) - 97;
+            if (!node.children[index]) {
+                return false;
+            }
+            node = node.children[index]
+        }
+
+        return node.endOfWord;
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+        let node = this.root;
+        for (let i = 0; i < prefix.length; i++) {
+            let index = prefix.charCodeAt(i) - 97;
+            if (!node.children[index]) {
+                return false;
+            }
+            node = node.children[index]
+        }
+
+        return true;
+    }
+}
+
+class TrieNode {
+    constructor() {
+        this.endOfWord = false;
+        this.children = new Array(26).fill(null);
+    }
+}
