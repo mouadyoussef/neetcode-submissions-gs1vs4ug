e8476b53f6c3dcf38488781c30bb6e737a3c76ad
Youssef Mouad <imouad.you@gmail.com>
2026-04-16 21:14:46 +0100

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if (nums.length === 0) return 0;
+        if (nums.length === 1) return nums[0];
+
+        const memo = [nums[0]];
+        memo[1] = Math.max(nums[0], nums[1]);
+
+        for (let i = 2; i < nums.length; i++) {
+            let max = Math.max(memo[i - 1], nums[i] + memo[i - 2])
+            memo[i] = max;
+        }
+
+        return Math.max(memo[nums.length - 1], memo[nums.length - 2])
+    }
+}
